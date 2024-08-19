b4e6243dca8a1f044573650d02d7d0b50e36037f
JustCloudy07 <gilbertgermo111@gmail.com>
2024-08-19 21:05:35 +0100

    feat: Implemented FPS system via the pygame object time.clock(). Adjusted game speed to 60FPS.

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         screen.fill((0,0,0), None, 0)
         pygame.display.flip()
@@ -14,6 +17,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
