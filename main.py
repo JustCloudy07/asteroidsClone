0304e3ec6b71823c78702367af6f899d173dd580
JustCloudy07 <gilbertgermo111@gmail.com>
2024-08-19 22:04:22 +0100

    Add: Added .gitignore to ignore __pycache__ and venv, added two classes (circleshape.py, and player.py), added PLAYER_RADIUS to constants.py, added player object to main.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 
 from constants import *
 
+from player import Player
+
+
 def main():
     print("Starting asteroids!")
     pygame.init()
@@ -10,15 +13,23 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    while True:
-        screen.fill((0,0,0), None, 0)
-        pygame.display.flip()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        
         
         dt = clock.tick(60) / 1000
+
+       
 
 if __name__ == "__main__":
     main()
